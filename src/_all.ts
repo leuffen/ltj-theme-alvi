d1a6cd2b21ddc81c0a1e3b153bea8cb267ba850e
Enzo Volkmann <enzo@volkmann.dev>
2023-12-05 08:55:05 +0100

    feat: add slider

--- src/_all.ts
+++ src/_all.ts
@@ -3,3 +3,4 @@ import "./sections/header1/header1";
 import "./sections/prenav/prenav";
 import "./sections/ribbons/ribbons";
 import "./sections/two-columns/two-columns";
+import "./sections/slider/slider";
