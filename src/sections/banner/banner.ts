3edb1fa3cf0e274f7faddde04af0bf0764ab6cec
Enzo Volkmann <enzo@volkmann.dev>
2023-12-08 09:36:02 +0100

    banner

--- src/sections/banner/banner.ts
+++ src/sections/banner/banner.ts
@@ -4,15 +4,15 @@ Joda.registerTemplate(
     "banner",
     // language=HTML
     `
-        <section class="tjs-banner py-5 container-fluid">
+        <section class="tjs-banner container-fluid">
             <div class="container">
                 <div class="row">
-                    <div class="d-none tjs-banner__image d-flex align-items-center :: col-12 mb-5 :md: col-3 justify-content-center mb-0">
-                        <slot data-select="img" data-replace></slot>
+                    <div class="d-none tjs-banner__image align-items-center :: col-12 mb-5 :md: col-3 justify-content-center mb-0">
+                        <slot data-select="img"></slot>
                     </div>
                     <div class=":: col-12 :md: col-9">
-                        <slot data-replace></slot>
-                        <slot data-select="a" data-replace data-child-class="alvi-btn outline white uppercase d-inline-flex mt-4"></slot>
+                        <slot></slot>
+                        <slot data-select="a" data-child-class="alvi-btn outline white uppercase d-inline-flex mt-4"></slot>
                     </div>
                 </div>
             </div>
@@ -27,6 +27,7 @@ Joda.registerTemplate(
 
             if (image) {
                 imageContainer.classList.remove("d-none");
+                imageContainer.classList.add("d-flex");
             }
         }
     }
