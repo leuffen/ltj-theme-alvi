ab3ad043626347be11c178bd289a468616e9d1f2
Enzo Volkmann <enzo@volkmann.dev>
2023-12-08 09:47:01 +0100

    cards

--- src/sections/cards/cards.ts
+++ src/sections/cards/cards.ts
@@ -3,7 +3,7 @@ import {Joda} from "@leuffen/jodastyle";
 Joda.registerTemplate("card",
     // language=HTML
     `
-        <div class="tjs-card :: col-12 :md: col-4">
+        <div class="tjs-card :: col-12 :lg: col-4">
             <div class="tjs-card__content">
                 <slot data-select="img"></slot>
                 <div class="tjs-card__content__text">
@@ -19,8 +19,8 @@ Joda.registerTemplate(
     `
         <section class="tjs-cards container-fluid">
             <div class="container">
-                <slot data-replace></slot>
-                <div class="tjs-cards__cards row">
+                <slot></slot>
+                <div class="tjs-cards__cards row mt-5">
                     <slot data-select=":scope > .children > *"  data-child-layout="use: #card"></slot>
                 </div>
             </div>
