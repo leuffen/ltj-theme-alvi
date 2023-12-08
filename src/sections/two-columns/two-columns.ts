bafcf252e93ed0f8395e780df7f750145e6cd413
Enzo Volkmann <enzo@volkmann.dev>
2023-12-08 09:30:00 +0100

    two columns

--- src/sections/two-columns/two-columns.ts
+++ src/sections/two-columns/two-columns.ts
@@ -7,11 +7,11 @@ Joda.registerTemplate(
         <section class="tjs-two-columns container">
             <div class="row">
                 <div class=":: col-12 :md: col-6 :lg: col-7">
-                    <slot data-replace></slot>
-                    <slot data-select="a" data-replace data-child-class="alvi-link-arrow"></slot>
+                    <slot></slot>
+                    <slot data-select="a" data-child-class="alvi-link-arrow d-inline-flex mt-4"></slot>
                 </div>
                 <div class=":: col-12 :md: col-6 :lg: col-5">
-                    <slot data-select="img" data-replace data-child-class=":: mt-3 :md: mt-0"></slot>
+                    <slot data-select="img" data-child-class=":: mt-3 :md: mt-0"></slot>
                 </div>
             </div>
         </section>
