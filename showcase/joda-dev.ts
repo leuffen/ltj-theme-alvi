ea8b2853684dadcafa6155470218bbb51adf011b
Matthias Leuffen <matthes@leuffen.de>
2023-12-12 10:46:49 +0100

    update

--- showcase/joda-dev.ts
+++ showcase/joda-dev.ts
@@ -4,6 +4,7 @@ import "@leuffen/jodastyle-dev/index.scss";
 
 import "@leuffen/jodastyle-dev";
 import "./../theme/dev";
-import {joda_dev_config} from "@leuffen/jodastyle-dev";
 
+
+import {joda_dev_config} from "@leuffen/jodastyle-dev";
 joda_dev_config.stylesheet = "/showcase/joda-dev.css";
