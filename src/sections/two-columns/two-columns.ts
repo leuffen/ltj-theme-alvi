40ab61b57c9fda7c936adc8f892660c73190c62e
Enzo Volkmann <enzo@volkmann.dev>
2024-02-05 08:57:27 +0100

    form checkbox

--- src/sections/two-columns/two-columns.ts
+++ src/sections/two-columns/two-columns.ts
@@ -7,7 +7,7 @@ Joda.registerTemplate(
         <section class="tjs__sec-card-2col [[layout.class]] :: mobile :lg: desktop">
             <div class="tjs__sec-card-2col--container [[layout.container]]">
                 <div class="row :: d-flex gap-5 :lg: gap-0">
-                    <div class="tjs__sec-card-2col--col-start tjs__section-text p-4 p-lg-5 :: col-12 :lg: col-[[layout.cols]]">
+                    <div class="tjs__sec-card-2col--col-start tjs__section-text p-4 p-lg-5 [[layout.col-1-class]] :: col-12 :lg: col-[[layout.cols]]">
                         <slot></slot>
                     </div>
                     <div class="tjs__sec-card-2col--col-end image-side :: col-12 :lg: col-[[12 - layout.cols]] ">
