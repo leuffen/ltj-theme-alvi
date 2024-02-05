3e9888c8b9274766e3f54802a859ba8bcb4f91e7
Enzo Volkmann <enzo@volkmann.dev>
2024-02-05 13:38:28 +0100

    gradient; color; spacing

--- src/sections/cards-2/cards-2.ts
+++ src/sections/cards-2/cards-2.ts
@@ -4,6 +4,7 @@ Joda.registerTemplate("cards-2",
     // language=HTML
     `
         <section class="tjs-cards-2 :: :md: md">
+            <div class="gradient [[layout.gradient ? '' : 'd-none']]"></div>
             <div class="container">
                 <div class="row">
                     <slot data-select=".children > *" data-child-class="tjs-card-2 :: col-12 :md: col-6"></slot>
