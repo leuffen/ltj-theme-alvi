0ea24ee6e77901cf385681fed3a41c966465c74a
Enzo Volkmann <enzo@volkmann.dev>
2024-02-29 15:54:10 +0100

    feat(cards): customize cols

--- src/sections/cards/cards.ts
+++ src/sections/cards/cards.ts
@@ -3,7 +3,7 @@ import {Joda} from "@leuffen/jodastyle";
 Joda.registerTemplate("card",
     // language=HTML
     `
-        <div class="tjs-card :: col-12 :lg: col-4">
+        <div class="tjs-card :: [[layout.colClassSm]] :md: [[layout.colClassMd]] :lg: [[layout.colClassLg]]">
             <div class="tjs-card__content">
                 <slot data-select="img"></slot>
                 <div class="tjs-card__content__text">
@@ -21,11 +21,18 @@ Joda.registerTemplate(
             <div class="container">
                 <slot></slot>
                 <div class="tjs-cards__cards row mt-5">
-                    <slot data-select=":scope > .children > *"  data-child-layout="use: #card"></slot>
+                    <slot
+                        data-select=":scope > .children > *"
+                        data-child-layout="use: #card; colClassSm: [[layout.colClassSm]]; colClassMd: [[layout.colClassMd]]; colClassLg: [[layout.colClassLg]]"
+                    ></slot>
                 </div>
             </div>
         </section>
     `,
-    {},
+    {
+        colClassSm: "col-12",
+        colClassMd: "col-12",
+        colClassLg: "col-4",
+    },
     {}
 );
